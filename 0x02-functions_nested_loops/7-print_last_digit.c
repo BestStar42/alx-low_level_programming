6791a5ea49c70dc18d75947da4a9d520e032c21a
BestStar42 <rajijamiu968@gmail.com>
2022-07-07 04:52:38 -0700

    Fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,11 @@ int print_last_digit(int n)
 {
 	int x;
 
+	if (a < 0)
+		a = -a;
 	if (n < 0)
 		n = -n;
-	x = n % 10;
-	if (a < 0)
-		a = -n;
+	x = n % 10;;
 	_putchar (x + '0');
 	return (x);
 }
