19bf9d5e8b02e1d2f17935f60894b2332dab66d8
BestStar42 <rajijamiu968@gmail.com>
2022-07-07 18:22:51 -0700

    fixed

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,6 +10,7 @@ int main(void)
 	long int n;
 	long int max;
 	long int i;
+
 	n = 612852475143;
 	max = -1;
 	while (n % 2 == 0)
@@ -17,7 +18,6 @@ int main(void)
 		max = 2;
 		n /= 2;
 	}
-
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
