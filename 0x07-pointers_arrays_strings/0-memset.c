c09aa866699b37c1efa69c626fe833f44f57c7d2
BestStar42 <rajijamiu968@gmail.com>
2022-07-18 08:03:48 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _memset - fill memory with a constant by te
@@ -13,9 +14,7 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int a;
 
 	for (a = 0; a < n; a++)
-	{
 		s[a] = b;
-	}
 
 	return (s);
 }
